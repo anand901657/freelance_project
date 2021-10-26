70bf396f80519fef8b054ea3d059147284092339
anand <anand901657@gmail.com>
2021-10-26 20:05:20 +0500

    Initial

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<html>
+    <head>
+        <title>Home Page</title>
+    </head>
+    <body>
+        <h1>Name: </h1>
+        <h1>Student ID: </h1>
+        <h1>Email: </h1>
+        <h1>Declaration: </h1>
+        <p>I declare that this assignment is my individual work. I have not
+work collaboratively nor have I copied from any other student's work or from any other source. I
+have not engaged another party to complete this assignment. I am aware of the University’s policy
+with regards to plagiarism. I have not allowed, and will not allow, anyone to copy my work with
+the intention of passing it off as his or her own work</p>
+        <a href="signup.php">SIGN UP</a>
+        <a href="login.php">LOGIN</a>
+        <a href="about.php">ABOUT</a>
+
+    </body>
+</html>
